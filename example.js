84e32bdbc721793ad3a56b7515bc07455279cde3
Viresh Shah <viresh.shah@autodesk.com>
2019-07-15 13:41:56 +0530

    Fixes for firefox v. > 64 and Safari.

--- example.js
+++ example.js
@@ -1,9 +1,8 @@
-$(function () {
-    $("div[href]").click(function (event) {
-        window.protocolCheck($(this).attr("href"),
-            function () {
-                alert("protocol not recognized");
-            });
-        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+$(function() {
+  $("a[href]").click(function(event) {
+    window.protocolCheck($(this).attr("href"), function() {
+      alert("This protocol is not supported.");
     });
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+  });
 });
